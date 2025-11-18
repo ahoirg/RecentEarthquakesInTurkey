109b5c423d5edab51019f5f7fe1d7faf416d2e1c
Ali <alihaydarozdag@gmail.com>
2025-11-19 00:56:34 +0100

    build(android): recreate Android folder for Flutter 3.38.2 compatibility

--- android/app/src/main/java/ahoirg/com/guncel_son_depremler/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package ahoirg.com.guncel_son_depremler;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/example/guncel_son_depremler/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.guncel_son_depremler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
